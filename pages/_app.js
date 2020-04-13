b362ac0a98335a6b17f3812ef01d0334c6c61b99
Yating(Esther) Su <yaringsu@foxmail.com>
2020-04-13 14:51:50 -0400

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+import App from "next/app";
+import Head from "next/head";
+import { AppProvider } from "@shopify/polaris";
+import "@shopify/polaris/styles.css";
+import translations from "@shopify/polaris/locales/en.json";
+
+class MyApp extends App {
+  state = {};
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Practice</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <AppProvider i18n={translations}>
+          <Component {...pageProps} />
+        </AppProvider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Layout, Page, TextStyle } from "@shopify/polaris";
+
+const Index = () => (
+  <div>
+    <Page>
+      <TextStyle variation="positive">
+        Sample app using Rect and Next.js
+      </TextStyle>
+    </Page>
+  </div>
+);
+
+export default Index;
